817a1dba50c6bbddfd6d94ceaaddfdf1a735d15b
Zullls <76924369+Zullls@users.noreply.github.com>
2021-08-07 17:46:01 +0300

    Delete test.php

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-                $userid = "665831333708300318";
-                $a = file_get_contents('https://api.void.cf/discord?type=user&id='.$userid);
-                $b = json_decode($a, true);
-                print($b['data']['avatarURL']);
-            ?>
